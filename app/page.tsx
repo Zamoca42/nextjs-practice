74767fc8e4af32b281f647d7e07b0f26491d01e0
Chooooooo <suntail2002@naver.com>
2024-03-30 16:49:45 +0900

    Chore: tailwind 설정 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 export default function Home() {
   return (
-    <main> </main>
+    <main className="h-screen flex items-center justify-center p-5">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col">
+        {["Name", "Email", "Password"].map((person, index) => (
+          <div key={index} className="flex items-center gap-5">
+            <div className="size-7 bg-blue-400 rounded-full" />
+            <span className="text-lg font-medium">{person}</span>
+            <div className="size-5 bg-red-500 text-white flex items-center justify-center rounded-full">
+              <span>{index}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
